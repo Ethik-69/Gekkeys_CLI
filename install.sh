908a488db702a31bd76a84b6e2f585b9804dd6b6
Ethik69 <ethancmk@hotmail.fr>
2018-08-21 13:53:37 +0200

    add chmo

--- install.sh
+++ install.sh
@@ -4,13 +4,14 @@
 apt-get update
 
 # Install python, pip
-apt-get install git python python3 python-pip python3-pip
+apt-get install python python3 python-pip python3-pip
 
 # Install needed packages
 pip3 install requirements.txt
 
 # Move gekkeys to the right folder
 mv gekkeys.py /usr/bin/gekkeys.py
+chmod +x /usr/bin/gekkeys.py
 
 # Create alias for cli
 echo alias gekkeys="/usr/bin/gekkeys.py" >> ~/.bashrc
