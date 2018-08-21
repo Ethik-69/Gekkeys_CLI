019c3fc09b327c3831f73f3874c60f26f097a201
Ethik69 <ethancmk@hotmail.fr>
2018-08-21 18:19:47 +0200

    bu !

--- install.sh
+++ install.sh
@@ -6,8 +6,9 @@ apt-get update
 # Install python, pip
 apt-get install python python3 python-pip python3-pip
 
+export LC_ALL=C
 # Install needed packages
-pip3 install requirements.txt
+pip3 install -r requirements.txt
 
 # Move gekkeys to the right folder
 mv gekkeys.py /usr/bin/gekkeys.py
